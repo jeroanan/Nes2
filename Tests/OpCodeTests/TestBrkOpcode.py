62cd14854a574b56b3c937f059d9067e782848b9
David Wilson <david.tosslehoff@gmail.com>
2014-10-28 20:33:50 +0000

    Splitting out unit tests

---
+++ Tests/OpCodeTests/TestBrkOpcode.py
@@ -0,0 +1,8 @@
+from Chip import OpCodes
+from Tests.OpCodeTests.OpCodeTestBase import OpCodeTestBase
+
+
+class TestBrkOpcode(OpCodeTestBase):
+
+    def test_brk_command_calls_brk_method(self):
+        self.assert_opcode_execution(OpCodes.brk_command, self.target.get_brk_command_executed)
